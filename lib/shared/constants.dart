6fe2a27840637bfdbb844576dc5eb0c74110a0cc
NirmalKAhirwar <ahirwarnirmal2017@gmail.com>
2022-12-23 05:33:54 +0530

    first commit:

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String apiKey = "AIzaSyDhnCNhaJaPhgysqwEQqOAUPmRqZrfYWag";
+  static String appId = "1:368379584463:web:6536c1afb4b28078277cba";
+  static String messagingSenderId = "368379584463";
+  static String projectId = "chatapp-62781";
+  final primaryColor =  const Color(0xFFee7b64);
+}
